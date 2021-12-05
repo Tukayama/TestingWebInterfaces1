c5cc9621ea292dff8c2e200c5f7a7abeed92b72f
Tukayama <ya.tumanov2009@yandex.ru>
2021-12-05 16:45:11 +0300

    init commit

--- src/test/java/ru/netology/web/DeditCardApplication.java
+++ src/test/java/ru/netology/web/DeditCardApplication.java
@@ -37,11 +37,74 @@ public class DeditCardApplication {
         driver.findElement(By.cssSelector("[type='tel']")).sendKeys("+79678883455");
         driver.findElement(By.cssSelector(".checkbox__box")).click();
         driver.findElement(By.cssSelector("button")).click();
-        String actualTest= driver.findElement(By.cssSelector(".Success_successBlock__2L3Cw")).getText().trim();
-        String expected ="Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
-        assertEquals(expected,actualTest);
+        String actualTest = driver.findElement(By.cssSelector(".Success_successBlock__2L3Cw")).getText().trim();
+        String expected = "Ваша заявка успешно отправлена! Наш менеджер свяжется с вами в ближайшее время.";
+        assertEquals(expected, actualTest);
 
     }
 
+    @Test
+    public void emptyNameField() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[type='text']")).sendKeys("");
+        driver.findElement(By.cssSelector("[type='tel']")).sendKeys("+79678883455");
+        driver.findElement(By.cssSelector(".checkbox__box")).click();
+        driver.findElement(By.cssSelector("button")).click();
+        String actualTest = driver.findElement(By.cssSelector(".input__sub")).getText().trim();
+        String expected = "Поле обязательно для заполнения";
+        assertEquals(expected, actualTest);
+
+    }
+
+    @Test
+    public void wrongPohneNumber() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[type='text']")).sendKeys("Иванов Иван");
+        driver.findElement(By.cssSelector("[type='tel']")).sendKeys("788875");
+        driver.findElement(By.cssSelector(".checkbox__box")).click();
+        driver.findElement(By.cssSelector("button")).click();
+        String actualTest = driver.findElement(By.cssSelector(" .input_invalid")).getText().trim();
+        String expected = "Мобильный телефон\nТелефон указан неверно. Должно быть 11 цифр, например, +79012345678.";
+        assertEquals(expected, actualTest);
+
+
+    }
+
+    @Test
+    public void emptyPohneField() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[type='text']")).sendKeys("Иванов Иван");
+        driver.findElement(By.cssSelector("[type='tel']")).sendKeys("");
+        driver.findElement(By.cssSelector(".checkbox__box")).click();
+        driver.findElement(By.cssSelector("button")).click();
+        String actualTest = driver.findElement(By.cssSelector(" .input_invalid")).getText().trim();
+        String expected = "Мобильный телефон\nПоле обязательно для заполнения";
+        assertEquals(expected, actualTest);
+    }
+
+    @Test
+    public void latinNameSurname() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[type='text']")).sendKeys("Ivanov Ivan");
+        driver.findElement(By.cssSelector("[type='tel']")).sendKeys("+79678883455");
+        driver.findElement(By.cssSelector(".checkbox__box")).click();
+        driver.findElement(By.cssSelector("button")).click();
+        String actualTest = driver.findElement(By.cssSelector(".input__sub")).getText().trim();
+        String expected = "Имя и Фамилия указаные неверно. Допустимы только русские буквы, пробелы и дефисы.";
+        assertEquals(expected, actualTest);
+    }
+
+    @Test
+    public void emptyForm() {
+        driver.get("http://localhost:9999");
+        driver.findElement(By.cssSelector("[type='text']")).sendKeys("");
+        driver.findElement(By.cssSelector("[type='tel']")).sendKeys("");
+        driver.findElement(By.cssSelector(".checkbox__box")).click();
+        driver.findElement(By.cssSelector("button")).click();
+        String actualTest = driver.findElement(By.cssSelector(".input__sub")).getText().trim();
+        String expected = "Поле обязательно для заполнения";
+        assertEquals(expected, actualTest);
+
+    }
 }
 
